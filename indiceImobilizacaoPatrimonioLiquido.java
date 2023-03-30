b7e27b135c9e5907b3e3a6b394288e0e24c145f7
jcamposmelo <jcamposmelo@yahoo.com.br>
2023-03-30 16:26:35 -0300

    Como calcular o Índice de Imbilização do PL?

---
+++ indiceImobilizacaoPatrimonioLiquido.java
@@ -0,0 +1,14 @@
+public class imobilizacaoInvestimento {
+	public static void main(String[] args) {
+		double ativoPermanente = 90000;
+		double totalAtivo = 1300000;
+
+		double imobilizacaoInvestimento;
+
+		imobilizacaoInvestimento = ativoPermanente / totalAtivo;
+
+		System.out.println("O índice calculado em Imobilização do Investimento é?  " + 
+				imobilizacaoInvestimento);
+	}
+
+}
